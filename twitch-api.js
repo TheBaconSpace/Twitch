83ff6b0a8a22035f6e0dda4e28e49709f30c4508
Bacon_Space <thespaceguild@gmail.com>
2023-10-06 18:05:56 -0400

    Update twitch-api.js

--- twitch-api.js
+++ twitch-api.js
@@ -1,26 +1,53 @@
-// Load Twitch Embed
-const twitchEmbed = document.createElement('iframe');
-twitchEmbed.src = 'https://player.twitch.tv/?channel=YOUR_CHANNEL_NAME'; // Replace with your channel name
-twitchEmbed.width = '100%';
-twitchEmbed.height = '480'; // Adjust the height as needed
-twitchEmbed.frameborder = '0';
-document.getElementById('twitch').appendChild(twitchEmbed);
-
 // Function to fetch and display emotes
-async function loadEmotes() {
-    const response = await fetch('https://emotes.adamcy.pl/v1/channel/Bacon_Space/emotes/all');
-    const emotesData = await response.json();
+function loadEmotes() {
+    // Replace 'YOUR_ENCODED_USERNAME' with the encoded username
+    const encodedUsername = encodeURIComponent('Afro');
+    const api_url = `https://emotes.adamcy.pl/v1/channel/${encodedUsername}/emotes/all`;
 
-    // Process the emotesData and display emotes in the chat section
-    const chatSection = document.getElementById('chat');
-    // You'll need to parse the emotesData and create HTML elements for the emotes here
+    fetch(api_url)
+        .then(response => response.json())
+        .then(data => {
+            const emoteContainer = document.getElementById('emoteContainer');
+            emoteContainer.innerHTML = '';
 
-    // Example: Displaying an emote
-    const emoteURL = emotesData.emotes[0].url;
-    const emoteImg = document.createElement('img');
-    emoteImg.src = emoteURL;
-    chatSection.appendChild(emoteImg);
+            data.forEach(emote => {
+                // Create a div for each emote
+                const emoteDiv = document.createElement('div');
+                emoteDiv.className = 'emote';
+
+                // Create an img element for the emote
+                const img = document.createElement('img');
+                img.src = emote.urls[0].url;
+                img.alt = emote.code;
+
+                // Append the img element to the emote div
+                emoteDiv.appendChild(img);
+
+                // Append the emote div to the emoteContainer
+                emoteContainer.appendChild(emoteDiv);
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching emotes:', error);
+        });
 }
 
-// Call the loadEmotes function to load emotes when the page loads
-loadEmotes();
+// Function to embed Twitch chat
+function embedTwitchChat() {
+    // Replace 'YOUR_CHANNEL_NAME' with your Twitch channel name
+    const channelName = 'Afro';
+
+    // Embed Twitch Chat
+    const twitchChatEmbed = document.createElement('iframe');
+    twitchChatEmbed.src = `https://www.twitch.tv/embed/${channelName}/chat`;
+    twitchChatEmbed.width = '100%';
+    twitchChatEmbed.height = '480'; // Adjust the height as needed
+    twitchChatEmbed.frameborder = '0';
+    document.getElementById('twitch-chat').appendChild(twitchChatEmbed);
+}
+
+// Call the loadEmotes and embedTwitchChat functions when the page loads
+window.onload = function () {
+    loadEmotes();
+    embedTwitchChat();
+};
