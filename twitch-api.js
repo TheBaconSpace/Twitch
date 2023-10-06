d9de9b475b7c8e6c0e69e56da560b47d0f15a3c9
Bacon_Space <thespaceguild@gmail.com>
2023-10-06 17:59:19 -0400

    Update twitch-api.js

--- twitch-api.js
+++ twitch-api.js
@@ -1,44 +1,26 @@
-// Replace with your Twitch client ID and Bearer token
-const clientId = '2oag632o0mp6n85h3mismr2ocsa1jip';
-const bearerToken = '62ly1aqjnymr30sn6yga5xpxuo4yi9';
+// Load Twitch Embed
+const twitchEmbed = document.createElement('iframe');
+twitchEmbed.src = 'https://player.twitch.tv/?channel=YOUR_CHANNEL_NAME'; // Replace with your channel name
+twitchEmbed.width = '100%';
+twitchEmbed.height = '480'; // Adjust the height as needed
+twitchEmbed.frameborder = '0';
+document.getElementById('twitch').appendChild(twitchEmbed);
 
-// Function to fetch Twitch stream data
-async function fetchTwitchData() {
-    try {
-        const response = await fetch('https://api.twitch.tv/helix/streams?user_login=Afro', {
-            headers: {
-                'Client-ID': clientId,
-                'Authorization': `Bearer ${bearerToken}`,
-            },
-        });
-        const data = await response.json();
+// Function to fetch and display emotes
+async function loadEmotes() {
+    const response = await fetch('https://emotes.adamcy.pl/v1/channel/Bacon_Space/emotes/all');
+    const emotesData = await response.json();
 
-        if (data.data && data.data.length > 0) {
-            // User is live
-            const streamData = data.data[0];
-            document.getElementById('status').textContent = 'Live';
-            document.getElementById('viewers').textContent = `Viewers: ${streamData.viewer_count}`;
-            document.getElementById('avatar').innerHTML = `<img src="${streamData.thumbnail_url.replace('{width}', '320').replace('{height}', '180')}">`;
-            // Calculate uptime from stream start time
-            const startTime = new Date(streamData.started_at);
-            const currentTime = new Date();
-            const uptimeSeconds = Math.floor((currentTime - startTime) / 1000);
-            const hours = Math.floor(uptimeSeconds / 3600);
-            const minutes = Math.floor((uptimeSeconds % 3600) / 60);
-            document.getElementById('uptime').textContent = `Uptime: ${hours}h ${minutes}m`;
-        } else {
-            // User is not live
-            document.getElementById('status').textContent = 'Offline';
-            document.getElementById('viewers').textContent = '';
-            document.getElementById('avatar').innerHTML = '';
-            document.getElementById('uptime').textContent = '';
-        }
-    } catch (error) {
-        console.error('Error fetching Twitch data:', error);
-    }
+    // Process the emotesData and display emotes in the chat section
+    const chatSection = document.getElementById('chat');
+    // You'll need to parse the emotesData and create HTML elements for the emotes here
+
+    // Example: Displaying an emote
+    const emoteURL = emotesData.emotes[0].url;
+    const emoteImg = document.createElement('img');
+    emoteImg.src = emoteURL;
+    chatSection.appendChild(emoteImg);
 }
 
-// Fetch Twitch data on page load
-fetchTwitchData();
-// Update every 5 minutes (300,000 milliseconds)
-setInterval(fetchTwitchData, 300000);
+// Call the loadEmotes function to load emotes when the page loads
+loadEmotes();
